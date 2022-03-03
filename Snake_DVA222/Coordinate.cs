5d611a0d7e43f443b1acdcb79370cdc2a20e3f39
Casper Norén <casper.noren01@gmail.com>
2022-03-03 13:27:19 +0100

    Added Coordinate class
    Coordinate class holds position in 2D array (the grid) as X and Y

---
+++ Snake_DVA222/Coordinate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_DVA222
+{
+    internal class Coordinate
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}

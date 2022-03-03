44081ea608dc97623e91f64f2a9021d4cf584882
Vestue <61783376+Vestue@users.noreply.github.com>
2022-03-03 09:52:38 +0100

    Add direction enum

--- Snake_DVA222/Movement.cs
+++ Snake_DVA222/Movement.cs
@@ -13,10 +13,10 @@ namespace Snake_DVA222
             // Kolla om snake är null
 
             // Change method names depending on implementation
-            if (key == ConsoleKey.UpArrow || key == ConsoleKey.W) snake.MoveUp;
-            else if (key == ConsoleKey.DownArrow || key == ConsoleKey.S) snake.MoveDown;
-            else if (key == ConsoleKey.LeftArrow || key == ConsoleKey.A) snake.MoveLeft;
-            else if (key == ConsoleKey.RightArrow || key == ConsoleKey.D) snake.MoveRight;
+            if (key == ConsoleKey.UpArrow || key == ConsoleKey.W) snake.Move(Direction.Up);
+            else if (key == ConsoleKey.DownArrow || key == ConsoleKey.S) snake.Move(Direction.Down);
+            else if (key == ConsoleKey.LeftArrow || key == ConsoleKey.A) snake.Move(Direction.Left);
+            else if (key == ConsoleKey.RightArrow || key == ConsoleKey.D) snake.Move(Direction.Right);
         }
     }
 }

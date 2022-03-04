ddc1d7e18a7019a57c7c4192df32ac82c8a56abb
Casper Norén <casper.noren01@gmail.com>
2022-03-04 17:10:17 +0100

    Created Grid class

---
+++ Snake_DVA222/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_DVA222
+{
+    internal class Grid
+    {
+        int Width, Height;
+        public Grid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}

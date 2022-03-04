dc03ecffdcb8aea79a88e2b1f154b1d7432dda91
cenza1 <100695978+cenza1@users.noreply.github.com>
2022-03-04 01:41:15 +0100

    Add files via upload

---
+++ Snake_DVA222/orangeFood.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace Snake_DVA222
+{
+    internal class orangeFood : IFood
+    {
+        Pen pen = new Pen(Color.Orange);
+        int value;
+        Rectangle Square = new Rectangle();
+        public orangeFood(int x, int y)
+        {
+            Square.Width = 50;
+            Square.Height = 50;
+            Square.X = x;
+            Square.Y = y;
+            value = 1;
+
+        }
+
+
+        public void Draw(Graphics g)
+        {
+            g.DrawRectangle(pen, Square);
+
+
+        }
+
+        public void TryHit(Snake snake)
+        {
+
+
+        }
+        public int returnPoints()
+        {
+
+            return value;
+        }
+    }
+}
---
+++ Snake_DVA222/yellowFood.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace Snake_DVA222
+{
+    internal class yellowFood : IFood
+    {
+        Pen pen = new Pen(Color.Yellow);
+        int value;
+        Rectangle Square = new Rectangle();
+        public yellowFood(int x, int y)
+        {
+            Square.Width = 50;
+            Square.Height = 50;
+            Square.X = x;
+            Square.Y = y;
+            value = -1;
+
+        }
+
+
+        public void Draw(Graphics g)
+        {
+
+            g.DrawRectangle(pen, Square);
+
+        }
+
+        
+        public int returnpoints()
+        {
+            value = 1;
+
+            return value;
+        }
+    }
+}

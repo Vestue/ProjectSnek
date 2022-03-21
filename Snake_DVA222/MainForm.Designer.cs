cc9d7eb95aeb774184ee3ea15debdd35d33e97f2
Vestue <61783376+Vestue@users.noreply.github.com>
2022-03-21 16:29:28 +0100

    Add labelBox for score

--- Snake_DVA222/MainForm.Designer.cs
+++ Snake_DVA222/MainForm.Designer.cs
@@ -34,6 +34,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ScoreDisplay = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,9 +42,10 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Chartreuse;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(298, 93);
+            this.button1.Location = new System.Drawing.Point(261, 70);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(199, 63);
+            this.button1.Size = new System.Drawing.Size(174, 47);
             this.button1.TabIndex = 0;
             this.button1.Text = "1 Player";
             this.button1.UseVisualStyleBackColor = false;
@@ -53,9 +55,10 @@
             // 
             this.button2.BackColor = System.Drawing.Color.MediumTurquoise;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(298, 181);
+            this.button2.Location = new System.Drawing.Point(261, 136);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(199, 63);
+            this.button2.Size = new System.Drawing.Size(174, 47);
             this.button2.TabIndex = 1;
             this.button2.Text = "2 Players";
             this.button2.UseVisualStyleBackColor = false;
@@ -64,9 +67,10 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(741, 446);
+            this.button3.Location = new System.Drawing.Point(648, 334);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(140, 58);
+            this.button3.Size = new System.Drawing.Size(122, 44);
             this.button3.TabIndex = 2;
             this.button3.Text = "Exit Game";
             this.button3.UseVisualStyleBackColor = true;
@@ -76,9 +80,10 @@
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(298, 12);
+            this.textBox1.Location = new System.Drawing.Point(261, 9);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(271, 61);
+            this.textBox1.Size = new System.Drawing.Size(238, 50);
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "Play Snake";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -88,24 +93,37 @@
             // 
             this.pictureBox1.Enabled = false;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, -77);
+            this.pictureBox1.Location = new System.Drawing.Point(10, -58);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(961, 680);
+            this.pictureBox1.Size = new System.Drawing.Size(841, 510);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // ScoreDisplay
+            // 
+            this.ScoreDisplay.AutoSize = true;
+            this.ScoreDisplay.BackColor = System.Drawing.Color.Transparent;
+            this.ScoreDisplay.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ScoreDisplay.Location = new System.Drawing.Point(10, 9);
+            this.ScoreDisplay.Name = "ScoreDisplay";
+            this.ScoreDisplay.Size = new System.Drawing.Size(59, 75);
+            this.ScoreDisplay.TabIndex = 5;
+            this.ScoreDisplay.Text = "label1\r\ntext\r\ntext";
+            this.ScoreDisplay.Visible = false;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 533);
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.ScoreDisplay);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "Project Snake";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -122,5 +140,6 @@
         private Button button3;
         private TextBox textBox1;
         private PictureBox pictureBox1;
+        private Label ScoreDisplay;
     }
 }
